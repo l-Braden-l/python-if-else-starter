5add0416b38f4ef6965940bd502154940061b48f
braden <leachbr787@northwested.org>
2024-10-01 13:24:35 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+#Braden Leach
+#Oct 1 2024
+#If else statements
+
+
+
+#task 1 
+#i read it
+#task 2 
+    #is green
+alien_color = 'green'
+if alien_color == 'green':
+    print('You earned 5 points for shooting the alien')
+else:
+    print('You earned  points for shooting the alien')
+    # is not green 
+alien_color = 'red'
+if alien_color == 'green':
+    print('You earned 5 points for shooting the alien')
+else:
+    print('You earned  points for shooting the alien')
+#task 3 
+name = input('write your first name:\n')
+if len(name) <= 5:
+    print('your name is too short')
+else:
+    print('You have a long name')
+#task 4 
+
+#task 5 
